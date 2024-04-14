8bb0bf99f9a1ce76682fb857e3735baaa028db9e
Sudha Yadav <150154402+sudhajobs0107@users.noreply.github.com>
2024-04-15 00:35:22 +0530

    Update Vote.js

--- frontend/src/components/Vote.js
+++ frontend/src/components/Vote.js
@@ -6,7 +6,7 @@ class Vote extends Component{
     super();
 
     //configure the APIHOSTPORT, this is the public IP address of the host that the API server is running on
-    this.APIHOSTPORT = `${process.env.REACT_APP_APIHOSTPORT}`;
+    this.APIHOSTPORT = 'Here add your API-EXTERNAL-IP'
 
     this.state = {
       vote: 0
@@ -18,7 +18,7 @@ class Vote extends Component{
   handleClick () {
     //implement the handleClick function which will be called when the user clicks on the voting button
     //this invokes an AJAX request to the API to vote on the current programming language
-    var url = `http://${this.APIHOSTPORT}/languages/${this.props.id}/vote`;
+    var url = `http://Here add your API-EXTERNAL-IP/languages/${this.props.id}/vote`;
     axios.get(url)
       .then(response => this.setState({vote: this.state.vote+1}))
   }
